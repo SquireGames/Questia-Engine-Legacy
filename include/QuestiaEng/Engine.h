9d83d78051c5ecfdb0279f8d0ab55a49218b4a19
Matthew Kaminski <kaminskimateusz7@gmail.com>
2017-03-12 20:08:03 -0400

    Started working on the engine

--- include/QuestiaEng/Engine.h
+++ include/QuestiaEng/Engine.h
@@ -1,17 +1,45 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include <SFML/Graphics/RenderWindow.hpp>
+
 #include "QuestiaEng/ResourceManager/ResourceManager.h"
+#include "QuestiaEng/Data/InputBuffer.h"
 #include "QuestiaEng/GuiManager/GuiManager.h"
 
 class Engine
 {
 public:
-	Engine();
-	~Engine();
+	//ctor + dtor
+	Engine(std::string windowName, int tickRate);
+	~Engine() = default;
+
+	//runtime loop function
+	bool run();
+	bool tick();
+	void processInput();
+	void render();
+	
+	//used in a state
+	void terminate() {toTerminate = true;}
+
+	//interfaces
+	ResourceManager& 	res() {return resourceManager;}
+	InputBuffer& 		inp() {return inputBuffer;}
+	GuiManager&			gui() {return guiManager;}
 
 private:
+	sf::RenderWindow window;
+
 	ResourceManager resourceManager;
+	InputBuffer inputBuffer;
+	GuiManager guiManager;
+
+	sf::Clock clock;
+	sf::Time timePerFrame;
+	sf::Time timeSinceLastTick;
+	
+	bool toTerminate = false;
 };
 
 #endif // ENGINE_H
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,11 +1,65 @@
 #include "QuestiaEng/Engine.h"
 
-Engine::Engine():
-	resourceManager()
+Engine::Engine(std::string windowName, int tickRate):
+	window(sf::VideoMode(1920, 1080), windowName, sf::Style::Default)
+	//managers
+	, resourceManager()
+	, guiManager(window, resourceManager)
+	//variables
+	, timePerFrame(sf::seconds(1.f/std::max(1, tickRate)))
 {
+	//TODO load from file
+	window.setVerticalSyncEnabled(true);
+	window.setView(sf::View(sf::Vector2f(window.getSize().x/2, window.getSize().y/2), sf::Vector2f(window.getSize())));
 }
 
-Engine::~Engine()
+bool Engine::run()
 {
+	return !toTerminate && window.isOpen();
 }
 
+bool Engine::tick()
+{
+	sf::Time elapsedTime = clock.restart();
+	timeSinceLastTick += elapsedTime;
+	return (timeSinceLastTick > timePerFrame) ? (timeSinceLastTick -= timePerFrame, true) : false;
+}
+
+void Engine::processInput()
+{
+	sf::Event event;
+
+	while(window.pollEvent(event))
+	{
+		switch(event.type)
+		{
+		case sf::Event::TextEntered:
+			{
+				inputBuffer.enqueue(event.text.unicode);
+			}
+			break;
+		case sf::Event::KeyPressed:
+			break;
+		case sf::Event::KeyReleased:
+			break;
+		case sf::Event::MouseWheelMoved:
+			break;
+		case sf::Event::Resized:
+			break;
+		case sf::Event::MouseButtonReleased:
+			break;
+		case sf::Event::MouseButtonPressed:
+			break;
+		case sf::Event::Closed:
+			window.close();
+			break;
+		}
+	}
+}
+
+void Engine::render()
+{
+	window.clear(sf::Color::White);
+	//...
+	window.display();
+}
